fa4973f362a5cc592e08dcc01bd9260a1659c8ab
Diego López <94648763+Didalz@users.noreply.github.com>
2025-07-30 19:02:55 -0500

    clasemiercoles.py

--- clasemiercoles.py
+++ clasemiercoles.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #NOMBRE DE LA PESTAÑA
 st.set_page_config(layout='centered',
